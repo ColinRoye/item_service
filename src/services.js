6c2475fdd62a503231ee7118b4a225aa7711ae22
ColinRoye <croye97@gmail.com>
2019-11-14 21:05:31 -0500

    added assertions for associated media

--- src/services.js
+++ src/services.js
@@ -2,44 +2,56 @@ const debug = require("./debug");
 const env = require("./env");
 const db = require("./database");
 const uuid = require("uuid/v1")
+const axios = require("axios");
 
 //export db agnostic services
 module.exports={
      getAll: async()=>{
-	   return await db.getAll();
+	      return await db.getAll();
      },
      addItem: async (content, childType, parent, media, username)=>{
-       let id = uuid();
-	  let ret = {};
+        let id = uuid();
+	      let ret = {};
 
-	  if(!content){
-	       ret.status = env.statusError
-	       return ret
-	  }
-          let item = {
-               content: content,
-               childType: childType,
-               username: username,
-               parent: parent,
-               media: media,
-               timestamp: (new Date() / 1000),
-               id: id
+	      if(!content){
+	          ret.status = env.statusError
+	          return ret
+        }
+        let item = {
+            content: content,
+            childType: childType,
+            username: username,
+            parent: parent,
+            media: media,
+            timestamp: (new Date() / 1000),
+            id: id
+        }
+        let url = env.baseUrl + "/used/" + media + "/"
+        let check = -1;
+        for(let i=0; i < media.length; i++){
+          check = (await axios.get('http://hackguy.cse356.compas.cs.stonybrook.edu' + "/used/"+media[i])).data.used
+
+          if(check !== '0'){
+              ret.status = env.statusError
+              return ret
+          }else{
+              check = (await axios.post('http://hackguy.cse356.compas.cs.stonybrook.edu' + "/used/"+media[i])).data.used
           }
 
-          ret = await db.addItem(item);
-          return ret;
-     },
-     getItemById: async (id)=>{
+        }
 
-          let ret = await db.getItemById(id);
-          debug.log("RETURNING ITEM: " + ret);
-          return ret;
-     },
-     getItemById: async (id)=>{
-
-          let ret = await db.getItemById(id);
-          debug.log("GETTING ITEM: " + ret);
-          return ret;
+        ret = await db.addItem(item);
+        return ret;
+    },
+    getItemById: async (id)=>{
+        let ret = await db.getItemById(id);
+        debug.log("RETURNING ITEM: " + ret);
+        return ret;
+    },
+    getItemById: async (id)=>{
+        let ret = await db.getItemById(id);
+        debug.log("GETTING ITEM: " + ret);
+        return ret;
      },
      deleteItemById: async (id, username)=>{
           let item = await db.getItemById(id);
